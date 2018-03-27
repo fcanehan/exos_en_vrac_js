151dd4d5f115d41c64f24719176d0034acbcff88
fabien <fabien@beweb-16.176N>
2018-03-27 12:34:01 +0200

    preparation jusqu'aux exos alpha

---
+++ public_html/stringFunctions.js
@@ -0,0 +1,8 @@
+function exerciceXX(){
+    
+}
+
+var div = document.getElementById("exoXX");
+var resultatDeLExo = "texte qui va bien";
+var texte = document.createTextNode(resultatDeLExo);
+div.appendChild(texte);
